662502366c7becd98cbf6763d5275710e9139271
pedrohfcarneiro <phtriplox8090@gmail.com>
2022-06-06 18:27:28 -0300

    add project to github

---
+++ Assets/FollowPath.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+            return;
+
+        //the node we are closest to at this moment
+        currentNode = g.getPathPoint(currentWP);
+
+        //if we are close enough to current waypoint move to the next
+        if(Vector3.Distance(g.getPathPoint(currentWP).transform.position , transform.position) < accuracy)
+        {
+            currentWP++;
+        }
+
+        //if we are not at the end of the path
+        if (currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), rotSpeed * Time.deltaTime);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+}
